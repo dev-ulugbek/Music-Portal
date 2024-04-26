91fa8bbaf3f6f09b1611306d251e4f11c9060b94
Ulugbek Hakimov <137672021+dev-ulugbek@users.noreply.github.com>
2024-04-26 09:50:00 +0500

    fix

--- Web_API/obj/Debug/net8.0/Web_API.AssemblyInfo.cs
+++ Web_API/obj/Debug/net8.0/Web_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c351ce5683156e576c0334a55ba474bffa9a13df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
